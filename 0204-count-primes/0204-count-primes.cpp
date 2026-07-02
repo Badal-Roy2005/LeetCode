c705d2869715d6ba564a7fcf32c77a2518ac284e
Badal Roy <broy52092@gmail.com>
2026-07-03 00:33:02 +0530

    Time: 163 ms (80.28%), Space: 12.8 MB (24.88%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -3,13 +3,13 @@ public:
 
    
     int countPrimes(int n) {
-
-    vector<bool> prime(n + 1, true);
+    if (n <= 2) return 0;
+    vector<bool> prime(n , true);
     prime[0] = false;
     prime[1] = false;
-     for(int i = 2; i * i <= n; i++){
+     for(int i = 2; i * i < n; i++){
         if(prime[i]){
-            for(int j = i * i ; j <= n;j+= i){
+            for(int j = i * i ; j < n;j+= i){
                 prime[j] = false;
             }
         }
